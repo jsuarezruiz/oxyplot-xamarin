3ff6e36c37c4ef5641f203dc2eaacd4674315775
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-08-14 21:29:20 +0200

    Created OxyPlot.Xamarin.Forms.Platform.GTK

---
+++ Source/Examples/Xamarin.Forms/SimpleDemo/SimpleDemo.GTK/SimpleDemo.GTK/Program.cs
@@ -0,0 +1,23 @@
+﻿using OxyPlot.Xamarin.Forms.GTK;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace SimpleDemo.GTK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            PlotViewRenderer.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
---
+++ Source/OxyPlot.Xamarin.Forms.GTK/PlotViewRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms.Platform.GTK;
+
+namespace OxyPlot.Xamarin.Forms.GTK
+{
+    public class PlotViewRenderer : ViewRenderer<PlotView, GtkSharp.PlotView>
+    {
+        static PlotViewRenderer()
+        {
+            Init();
+        }
+
+        public PlotViewRenderer()
+        {
+            // Do not delete
+        }
+
+        public static void Init()
+        {
+            PlotView.IsRendererInitialized = true;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<PlotView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+
+            var plotView = new GtkSharp.PlotView
+            {
+                Model = Element.Model,
+                Controller = Element.Controller
+            };
+
+            SetNativeControl(plotView);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Element == null || Control == null)
+            {
+                return;
+            }
+
+            if (e.PropertyName == PlotView.ModelProperty.PropertyName)
+            {
+                Control.Model = Element.Model;
+            }
+
+            if (e.PropertyName == PlotView.ControllerProperty.PropertyName)
+            {
+                Control.Controller = Element.Controller;
+            }
+        }
+    }
+}
